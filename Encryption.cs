d4e59656b2990482cc0fda1e49072feeb1e8dfbc
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2021-09-09 09:38:08 -0700

    Initial release

---
+++ Encryption.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChillDecrypter
+{
+    class Encryption
+    {
+        public static byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes, byte[] SaltBytes)
+        {
+            byte[] decryptedBytes = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, SaltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+        public static void DecryptFile(string file, byte[] PasswordBytes, byte[] SaltBytes)
+        {
+            byte[] BytesToBeDecrypted = File.ReadAllBytes(file);
+            
+            byte[] BytesDecrypted = AES_Decrypt(BytesToBeDecrypted, PasswordBytes, SaltBytes);
+
+            File.WriteAllBytes(file, BytesDecrypted);
+            string result = file.Substring(0, file.Length - Path.GetExtension(file).Length);
+            File.Move(file, result);
+            Console.Title = $"Chill Decryptor | {++DecryptedFiles} files decrypted";
+        }
+
+        static ulong DecryptedFiles = 0;
+
+        public static void DecryptDirectory(ref byte[] PwBytes, ref string location, ref byte[] SaltBytes)
+        {
+            string[] files = Directory.GetFiles(location);
+            string[] childDirectories = Directory.GetDirectories(location);
+            for (int i = 0; i < files.Length; i++)
+            {
+                string extension = Path.GetExtension(files[i]);
+                if (extension == Settings.EncryptedFilesExtension)
+                {
+                    try
+                    {
+                        DecryptFile(files[i], PwBytes, SaltBytes);
+                        Console.WriteLine($"Successfully decrypted {files[i]}");
+                    }
+                    catch { }
+                    //Console.WriteLine("Decrypted file from path: ");
+                }
+            }
+            for (int i = 0; i < childDirectories.Length; i++)
+            {
+                DecryptDirectory(ref PwBytes, ref childDirectories[i], ref SaltBytes);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ChillDecrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+   _____ _     _ _ _ _____                             _            
+  / ____| |   (_) | |  __ \                           | |           
+ | |    | |__  _| | | |  | | ___  ___ _ __ _   _ _ __ | |_ ___ _ __ 
+ | |    | '_ \| | | | |  | |/ _ \/ __| '__| | | | '_ \| __/ _ \ '__|
+ | |____| | | | | | | |__| |  __/ (__| |  | |_| | |_) | ||  __/ |   
+  \_____|_| |_|_|_|_|_____/ \___|\___|_|   \__, | .__/ \__\___|_|   
+                                            __/ | |                 
+                                           |___/|_|                 ");
+            Console.Title = "ChillDecryptor";
+
+            //Remove \n that might be inserted by mistake
+            Console.WriteLine("Please enter your key");
+            string key = Console.ReadLine().Replace("\n","");
+            Console.WriteLine("Please enter the salt");
+            string salt = Console.ReadLine().Replace("\n", "");
+
+            byte[] SaltBytes = Convert.FromBase64String(salt);
+            byte[] PasswordBytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(key));
+
+            foreach (DriveInfo d in DriveInfo.GetDrives())
+            {
+                if (d.IsReady == true)
+                {
+                    string DriveName = d.Name;
+                    try { Encryption.DecryptDirectory(ref PasswordBytes, ref DriveName, ref SaltBytes); } catch { }    
+                }
+            }
+        }
+
+
+    }
+}
